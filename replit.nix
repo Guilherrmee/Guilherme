0bfe658980398b6f44821ced977494f2b6a5c794
Guilherrmee <gd012938@gmail.com>
2024-10-29 19:33:15 -0300

    first commit

--- replit.nix
+++
@@ -1,6 +0,0 @@
-{pkgs}: {
-  deps = [
-    pkgs.imagemagick_light
-    pkgs.gh
-   ];
-}
--- src/models/userModel.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> refs/remotes/origin/main
